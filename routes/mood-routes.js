8e317da3ed3259161e1bdabf0dcb0383810735ff
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-12-07 10:00:29 +0000

    commit

--- routes/mood-routes.js
+++ routes/mood-routes.js
@@ -24,15 +24,12 @@ router.put('/user/:Id/mood', (req, res) =>{
 
 
 // RETRIEVE THE MOOD OF THE DAY CHOSEN BY ONE USER
-router.get('/user/:Id/mood', (req, res) =>{
-  let userId = req.params.Id;
+router.get('/user/:id/mood', (req, res) =>{
+  let userId = req.params.id;
   console.log(`PROBLEMMMMMMMMM`, userId)
 User.findById(userId)
-.then((resultFromId) => {
-  console.log(`resultFromId ${resultFromId}`);
-})
-  //.then((user) => {
-  //  console.log("The user for who we want to find the mood", user)
+.then((user) => {
+  console.log("The user for who we want to find the mood", user)
  
    //let index = user.history.length;
    //let LastMood = user.history[index];
@@ -44,7 +41,7 @@ User.findById(userId)
  //  return
   //  })
   .catch((err) => {
-    res.render('error', {err})
+    res.json('error', {err})
   });
 }); 
 

18d41005791d1f29ac98f047b81947cd2919a1f3
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-12-10 15:13:08 +0000

    build commited

--- app.js
+++ app.js
@@ -37,10 +37,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 app.use(session({
   secret: 'projectapp',
